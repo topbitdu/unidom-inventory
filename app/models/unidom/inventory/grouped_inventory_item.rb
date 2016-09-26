ddc8656a0475e3b41dba2b8773c14736c9865e27
Topbit Du <topbit.du@gmail.com>
2016-09-26 11:56:23 +0800

    1, Improve the Grouped Inventory Item model to include the As Inventory Item concern.

--- app/models/unidom/inventory/grouped_inventory_item.rb
+++ app/models/unidom/inventory/grouped_inventory_item.rb
@@ -4,9 +4,7 @@ class Unidom::Inventory::GroupedInventoryItem < ActiveRecord::Base
   self.table_name = 'unidom_grouped_inventory_items'
 
   include Unidom::Common::Concerns::ModelExtension
-
-  belongs_to :stored, polymorphic: true
-  belongs_to :store,  polymorphic: true
+  include Unidom::Inventory::Concerns::AsInventoryItem
 
   validates :quantity, presence: true, numericality: true
 
