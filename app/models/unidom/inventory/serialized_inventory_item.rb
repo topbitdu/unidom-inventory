80e65fce59040095260881ab203037ddac1ba30d
Topbit Du <topbit.du@gmail.com>
2016-09-27 09:47:27 +0800

    1, Improve the Serialized Inventory Item model to include the As Inventory Item concern.

--- app/models/unidom/inventory/serialized_inventory_item.rb
+++ app/models/unidom/inventory/serialized_inventory_item.rb
@@ -4,9 +4,7 @@ class Unidom::Inventory::SerializedInventoryItem < ActiveRecord::Base
   self.table_name = 'unidom_serialized_inventory_items'
 
   include Unidom::Common::Concerns::ModelExtension
-
-  belongs_to :stored, polymorphic: true
-  belongs_to :store,  polymorphic: true
+  include Unidom::Inventory::Concerns::AsInventoryItem
 
   validates :serial_number, presence: true, length: { in: 2..columns_hash['serial_number'].limit }
 
