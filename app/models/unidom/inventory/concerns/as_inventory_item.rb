8267ac5eb9a02281a8260f35ab7d5e05e6a12baa
Topbit Du <topbit.du@gmail.com>
2016-10-12 09:56:41 +0800

    1, Improve the As Inventory Item concern to add the #is_adjusted! method.

--- app/models/unidom/inventory/concerns/as_inventory_item.rb
+++ app/models/unidom/inventory/concerns/as_inventory_item.rb
@@ -11,6 +11,20 @@ module Unidom::Inventory::Concerns::AsInventoryItem
     has_many :pick_items, class_name: 'Unidom::Inventory::PickItem',              as: :inventory_item
     has_many :variances,  class_name: 'Unidom::Inventory::InventoryItemVariance', as: :inventory_item
 
+    def is_adjusted!(quantity: nil, due_to: nil, at: Time.now, description: nil, instruction: nil)
+      if respond_to? :quantity
+        increment! :quantity, quantity
+      else
+        if quantity.nil?
+          quantity = -1
+          soft_destroy
+        else
+          raise ArgumentError.new('The quantity should be nil when Inventory Item Variance is adjusted.')
+        end
+      end
+      variances.create! inventory_item: self, reason: due_to, quantity: quantity, description: description, instruction: instruction, opened_at: at
+    end
+
   end
 
   module ClassMethods
