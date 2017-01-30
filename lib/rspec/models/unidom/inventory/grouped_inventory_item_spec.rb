b388236d588e6de51f43e76865314d3aa0965e4c
Topbit Du <topbit.du@gmail.com>
2017-01-30 10:40:27 +0800

    1, Grouped Inventory Item spec.

---
+++ lib/rspec/models/unidom/inventory/grouped_inventory_item_spec.rb
@@ -0,0 +1,24 @@
+describe Unidom::Inventory::GroupedInventoryItem, type: :model do
+
+  before :each do
+  end
+
+  after :each do
+  end
+
+  context do
+
+    model_attributes = {
+      stored_id:   SecureRandom.uuid,
+      stored_type: 'Unidom::Inventory::Stored::Mock',
+      store_id:    SecureRandom.uuid,
+      store_type:  'Unidom::Inventory::Store::Mock',
+      lot_id:      SecureRandom.uuid,
+      quantity:    120.000000
+    }
+
+    it_behaves_like 'Unidom::Common::Concerns::ModelExtension', model_attributes
+
+  end
+
+end
