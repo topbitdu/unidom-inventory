3b8c12aded7a8faeaf58caba8a4cabc22f0a08f6
Topbit Du <topbit.du@gmail.com>
2017-02-05 11:32:51 +0800

    1, Pick List spec.

---
+++ lib/rspec/models/unidom/inventory/pick_list_spec.rb
@@ -0,0 +1,18 @@
+describe Unidom::Inventory::PickList, type: :model do
+
+  before :each do
+  end
+
+  after :each do
+  end
+
+  context do
+
+    model_attributes = {
+    }
+
+    it_behaves_like 'Unidom::Common::Concerns::ModelExtension', model_attributes
+
+  end
+
+end
