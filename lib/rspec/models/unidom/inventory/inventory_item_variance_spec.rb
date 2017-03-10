576b06fcc168475ad833914c3c848c26ffad8742
Topbit Du <topbit.du@gmail.com>
2017-03-10 12:32:53 +0800

    1, Improve the Inventory Item Variance spec for the validations on the #quantity attribute.

--- lib/rspec/models/unidom/inventory/inventory_item_variance_spec.rb
+++ lib/rspec/models/unidom/inventory/inventory_item_variance_spec.rb
@@ -18,6 +18,28 @@ describe Unidom::Inventory::InventoryItemVariance, type: :model do
 
     it_behaves_like 'Unidom::Common::Concerns::ModelExtension', model_attributes
 
+    it_behaves_like 'validates', model_attributes, :quantity,
+      {                               } => 0,
+      { quantity: nil                 } => 2,
+      { quantity: ''                  } => 2,
+      { quantity: '1'                 } => 0,
+      { quantity: 1                   } => 0,
+      { quantity: 'A'                 } => 1,
+      { quantity: '0.00'              } => 0,
+      { quantity: 0.00                } => 0,
+      { quantity: '-999_999_999.99'   } => 0,
+      { quantity: -999_999_999.99     } => 0,
+      { quantity: '-1_000_000_000'    } => 1,
+      { quantity: -1_000_000_000      } => 1,
+      { quantity: '-1_000_000_000.01' } => 1,
+      { quantity: -1_000_000_000.01   } => 1,
+      { quantity: '999_999_999.99'    } => 0,
+      { quantity: 999_999_999.99      } => 0,
+      { quantity: '1_000_000_000'     } => 1,
+      { quantity: 1_000_000_000       } => 1,
+      { quantity: '1_000_000_000.01'  } => 1,
+      { quantity: 1_000_000_000.01    } => 1
+
   end
 
 end
