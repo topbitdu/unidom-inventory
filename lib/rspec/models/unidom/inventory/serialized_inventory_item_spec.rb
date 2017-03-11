b9c0294fbe06053ca23e3bfd0485c57f537b73c8
Topbit Du <topbit.du@gmail.com>
2017-03-11 12:47:29 +0800

    1, Improve the Serialized Inventory Item spec for the validations on the #serial_number attribute.

--- lib/rspec/models/unidom/inventory/serialized_inventory_item_spec.rb
+++ lib/rspec/models/unidom/inventory/serialized_inventory_item_spec.rb
@@ -17,7 +17,26 @@ describe Unidom::Inventory::SerializedInventoryItem, type: :model do
       serial_number: '123456789012'
     }
 
+    serial_number_max_length = described_class.columns_hash['serial_number'].limit
+
     it_behaves_like 'Unidom::Common::Concerns::ModelExtension', model_attributes
+
+    it_behaves_like 'validates', model_attributes, :serial_number,
+      {                      } => 0,
+      { serial_number: nil   } => 2,
+      { serial_number: ''    } => 2,
+      { serial_number: '1'   } => 1,
+      { serial_number: '11'  } => 0,
+      { serial_number: '111' } => 0,
+      { serial_number: 'A'   } => 1,
+      { serial_number: 'AA'  } => 0,
+      { serial_number: 'AAA' } => 0,
+      { serial_number: '1'*(serial_number_max_length-1) } => 0,
+      { serial_number: '1'*serial_number_max_length     } => 0,
+      { serial_number: '1'*(serial_number_max_length+1) } => 1,
+      { serial_number: 'A'*(serial_number_max_length-1) } => 0,
+      { serial_number: 'A'*serial_number_max_length     } => 0,
+      { serial_number: 'A'*(serial_number_max_length+1) } => 1
 
   end
 
