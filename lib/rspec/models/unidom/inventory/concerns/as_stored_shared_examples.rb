b505b9ec90a49592139b1db70bbdca36ef8298ce
Topbit Du <topbit.du@gmail.com>
2017-04-14 11:41:53 +0800

    1, As Stored shared examples.

---
+++ lib/rspec/models/unidom/inventory/concerns/as_stored_shared_examples.rb
@@ -0,0 +1,45 @@
+shared_examples 'Unidom::Inventory::Concerns::AsStored' do |model_attributes|
+
+  before :each do
+  end
+
+  after :each do
+  end
+
+  context do
+
+    grouped_inventory_item_1_attributes = {
+      store_id:   SecureRandom.uuid,
+      store_type: 'Unidom::Inventory::Stored::Mock',
+      lot_id:     SecureRandom.uuid,
+      quantity:   10.00
+    }
+
+    grouped_inventory_item_2_attributes = {
+      store_id:   SecureRandom.uuid,
+      store_type: 'Unidom::Inventory::Stored::Mock',
+      lot_id:     SecureRandom.uuid,
+      quantity:   20.00
+    }
+
+    it_behaves_like 'has_many', model_attributes, :grouped_inventory_items, Unidom::Inventory::GroupedInventoryItem, [ grouped_inventory_item_1_attributes, grouped_inventory_item_2_attributes ]
+
+    serialized_inventory_item_1_attributes = {
+      store_id:      SecureRandom.uuid,
+      store_type:    'Unidom::Inventory::Stored::Mock',
+      lot_id:        SecureRandom.uuid,
+      serial_number: SecureRandom.hex(16)
+    }
+
+    serialized_inventory_item_2_attributes = {
+      store_id:      SecureRandom.uuid,
+      store_type:    'Unidom::Inventory::Stored::Mock',
+      lot_id:        SecureRandom.uuid,
+      serial_number: SecureRandom.hex(16)
+    }
+
+    it_behaves_like 'has_many', model_attributes, :serialized_inventory_items, Unidom::Inventory::SerializedInventoryItem, [ serialized_inventory_item_1_attributes, serialized_inventory_item_2_attributes ]
+
+  end
+
+end
