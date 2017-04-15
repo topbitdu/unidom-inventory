1ebf7e648dfce54ce953561905c29b2fb1fe62d8
Topbit Du <topbit.du@gmail.com>
2017-04-15 13:45:29 +0800

    1, Improve the RSpec shared examples manifest to require the As Stored shared examples.

--- lib/unidom/inventory/rspec_shared_examples.rb
+++ lib/unidom/inventory/rspec_shared_examples.rb
@@ -1,2 +1,3 @@
 require 'rspec/models/unidom/inventory/concerns/as_inventory_item_shared_examples'
 require 'rspec/models/unidom/inventory/concerns/as_store_shared_examples'
+require 'rspec/models/unidom/inventory/concerns/as_stored_shared_examples'
