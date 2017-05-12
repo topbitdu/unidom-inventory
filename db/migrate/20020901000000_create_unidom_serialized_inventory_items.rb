7c37db96334040680bd4c8463028bff036ed0ea2
Topbit Du <topbit.du@gmail.com>
2017-05-12 10:04:38 +0800

    1, Improve the Serialized Inventory Item migration for the #opened_at column, & the #closed_at column.

--- db/migrate/20020901000000_create_unidom_serialized_inventory_items.rb
+++ db/migrate/20020901000000_create_unidom_serialized_inventory_items.rb
@@ -13,8 +13,8 @@ class CreateUnidomSerializedInventoryItems < ActiveRecord::Migration
       t.string :serial_number, null: false, default: nil, limit: 200
 
       t.column   :state, 'char(1)', null: false, default: 'C'
-      t.datetime :opened_at,        null: false, default: Time.utc(1970)
-      t.datetime :closed_at,        null: false, default: ::Time.utc(3000)
+      t.datetime :opened_at,        null: false, default: Unidom::Common::OPENED_AT
+      t.datetime :closed_at,        null: false, default: Unidom::Common::CLOSED_AT
       t.boolean  :defunct,          null: false, default: false
       t.jsonb    :notation,         null: false, default: {}
 
