2f16b78d02aacf66a0a850a1754959e06cdc30f7
Topbit Du <topbit.du@gmail.com>
2017-05-13 13:44:39 +0800

    1, Improve the Grouped Inventory Item migration for the #opened_at column, & the #closed_at column.

--- db/migrate/20020902000000_create_unidom_grouped_inventory_items.rb
+++ db/migrate/20020902000000_create_unidom_grouped_inventory_items.rb
@@ -13,8 +13,8 @@ class CreateUnidomGroupedInventoryItems < ActiveRecord::Migration
       t.decimal :quantity, null: false, default: 0.0, precision: 12, scale: 2
 
       t.column   :state, 'char(1)', null: false, default: 'C'
-      t.datetime :opened_at,        null: false, default: ::Time.utc(1970)
-      t.datetime :closed_at,        null: false, default: ::Time.utc(3000)
+      t.datetime :opened_at,        null: false, default: Unidom::Common::OPENED_AT
+      t.datetime :closed_at,        null: false, default: Unidom::Common::CLOSED_AT
       t.boolean  :defunct,          null: false, default: false
       t.jsonb    :notation,         null: false, default: {}
 
